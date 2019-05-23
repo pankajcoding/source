0190cbb59004b72daacc3d1db6b394a239d38d51
pankajcoding <pankajkumar196yahoo@gmail.com>
2019-05-23 18:00:07 +0530

    ctci trees

--- ctci/tree/2sortedArrayToTree.py
+++ ctci/tree/2sortedArrayToTree.py
@@ -4,6 +4,23 @@ class Node:
         self.left=None
         self.right=None
 
+def levelOrder(root):
+    if not root:
+        return
+    queue=[root,None]
+    i=0
+    while len(queue)>1:
+        node=queue.pop(0)
+        if not node:
+            queue.append(None)
+            print()
+        else:
+            print(node.data,end="  ")
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    
 
 def MakeTree(A):
     n=len(A)
@@ -15,7 +32,8 @@ def MakeTree(A):
         mid=n//2
         node=Node(A[mid])
         node.left=MakeTree(A[0:mid])
-        if mid+1<n:
-            node.right=MakeTree(A[mid+1:])
+        # if mid+1<n:
+        node.right=MakeTree(A[mid+1:])
         return node
-MakeTree([1,2,3,4,5,6,7])
+root=MakeTree([1,2,3,4,5,6,7])
+levelOrder(root)
